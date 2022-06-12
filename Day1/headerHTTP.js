d1ef082117e17d3f1ce46b7d9b212ad9f3661b7e
rautrupesh1 <iamrupeshraut1@gmail.com>
2022-06-12 23:04:02 +0545

    Add an HTTP header

--- Day1/headerHTTP.js
+++ Day1/headerHTTP.js
@@ -1,7 +1,8 @@
 var http = require('http');
 http.createServer(function(req,res)
 {
-    res.writeHead(200, {'Content-T  ype': 'text/html'});
+    res.writeHead(200, {'Content-Type': 'text/html'}); // res.writeHead() is the status code
+    //200 means that all is ok, second arguement is an object containing the response header
     res.write("Hello World");
     res.end();
 }).listen(8080);
