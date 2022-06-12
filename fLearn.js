c47e7e99d71079aee26884751d6880d54adcebc2
rautrupesh1 <iamrupeshraut1@gmail.com>
2022-06-12 19:00:26 +0545

    creating first line of code that output hello world

--- fLearn.js
+++ fLearn.js
@@ -1,1 +1,6 @@
-console.log(module);
+var http = require("http"); // 
+http.createServer(function(req,res)
+{
+    res.writeHead(200, {'content-Type': 'text/html'});
+    res.end('Hello World!');
+}).listen(8080);
