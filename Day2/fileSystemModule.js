96b449cc7081a60afe608da411cc72be9b5c82d3
rautrupesh1 <iamrupeshraut1@gmail.com>
2022-06-13 13:34:49 +0545

    file system module

---
+++ Day2/fileSystemModule.js
@@ -0,0 +1,14 @@
+// Node.js file system module allows to work with the file system of computer 
+// to include file system module we use require() function method;
+
+var fs = require('fs');
+
+//Common uses of file system module:
+
+/*
+    Read Files
+    Create Files
+    Update Files
+    Delete Fiels
+    Rename Files
+*/
